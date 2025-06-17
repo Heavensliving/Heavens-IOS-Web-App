2fd27e91b9db1410abd6c18a0917792185f26635
Goobright <jaganprasad093@gmail.com>
2025-06-17 16:25:40 +0530

    added keystore file and andriod file

--- lib/view/splash_screen/SplashScreen.dart
+++ lib/view/splash_screen/SplashScreen.dart
@@ -107,11 +107,11 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   void _navigateToSignIn(BuildContext context) {
-    // Navigator.pushReplacementNamed(
-    //   context,
-    //   "/signin",
-    // );
-    Navigator.pushNamed(context, "/getstarted");
+    Navigator.pushReplacementNamed(
+      context,
+      "/signin",
+    );
+    // Navigator.pushNamed(context, "/getstarted");
   }
 
   @override
